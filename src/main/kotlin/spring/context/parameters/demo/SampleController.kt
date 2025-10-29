41866776531a719526b33163e240a590691619a0
Ruslan Ibragimov <ruslan@heapy.io>
2025-10-29 12:05:28 +0300

    Add request parameter reproducer

--- src/main/kotlin/spring/context/parameters/demo/SampleController.kt
+++ src/main/kotlin/spring/context/parameters/demo/SampleController.kt
@@ -2,6 +2,7 @@ package spring.context.parameters.demo
 
 import org.springframework.http.HttpMethod
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,4 +17,18 @@ class SampleController {
     context(httpMethod: HttpMethod)
     @GetMapping("/context-parameter")
     fun exampleContextParameter() = "Hello $httpMethod"
+
+    // This method works as expected on Spring Boot 3.1.3
+    context(httpMethod: HttpMethod)
+    @GetMapping("/context-parameter-with-param-explicit-name")
+    fun exampleContextParameterWithParamExplicitName(
+        @RequestParam("name") name: String,
+    ) = "Hello $name $httpMethod"
+
+    // This method fails on Spring Boot 3.1.3
+    context(httpMethod: HttpMethod)
+    @GetMapping("/context-parameter-with-param")
+    fun exampleContextParameterWithParam(
+        @RequestParam name: String,
+    ) = "Hello $name $httpMethod"
 }
