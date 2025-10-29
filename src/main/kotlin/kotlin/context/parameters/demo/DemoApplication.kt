6f5130516eec044a1d4b4e2aa67f2b38c77a83f4
Ruslan Ibragimov <ruslan@heapy.io>
2025-10-29 11:30:15 +0300

    Init

---
+++ src/main/kotlin/kotlin/context/parameters/demo/DemoApplication.kt
@@ -0,0 +1,11 @@
+package kotlin.context.parameters.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
